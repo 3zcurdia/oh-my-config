ad770d5e867f6a24aa56896de37f560d73a4f4f4
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2023-11-23 18:51:39 -0600

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,6 @@ DOT_ZSHRC=~/.zshrc
 SHELL=`which zsh`
 if [ ! -d "$OH_MY_ZSH_DIR" ]
 then
-
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
   
   plugin_output="$(sed -n '/^plugin/p' $DOT_ZSHRC)"
@@ -19,17 +18,15 @@ then
   editor_output="$(sed -n '/^EDITOR/p' $DOT_ZSHRC)"
   if [ ! -z $editor_output ]
   then
-    echo "EDITOR=code" >> $DOT_ZSHRC
+    echo "EDITOR=vi" >> $DOT_ZSHRC
   fi
-
-  if [ -z "$(chsh -s $SHELL)" ]
-  then
-    echo "SHELL has been changed to $SHELL"
-  fi
-
 else
   echo "$OH_MY_ZSH_DIR exists!, I can not overwrite this automatically."
 fi
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+curl https://raw.githubusercontent.com/3zcurdia/oh-my-config/main/Brewfile -o Brewfile
+brew bundle install
 
 GITCONFIG_DST=~/.gitconfig
 GITCOFIG_REMOTE="https://raw.github.com/3zcurdia/oh-my-config/master/dotgitconfig"
